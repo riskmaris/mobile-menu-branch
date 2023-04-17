7ff733e667236dfbee61bebb686c0cbccd92081a
riskmaris <mariskaps20@gmail.com>
2023-04-17 23:17:59 +0300

    make changes and add files

---
+++ js-app.js
@@ -0,0 +1,22 @@
+let menuBar = document.querySelector('#menu-bar');
+let xIcon = document.querySelector('#x-icon');
+let popupList = document.querySelector('.popup-list');
+
+xIcon.style.display = 'none'
+popupList.style.display = 'none'
+
+menuBar.addEventListener('click', () => {
+    xIcon.style.display = 'block'
+    popupList.style.display = 'block'
+    menuBar.style.display = 'none'
+});
+
+ xIcon.addEventListener('click', () => {
+    menuBar.style.display = 'block'
+    popupList.style.display = 'none'
+    xIcon.style.display = 'none'
+});
+
+
+
+
