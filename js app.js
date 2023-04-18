edaceccb126a22aaffa6a6cfa8779686e89322c1
riskmaris <mariskaps20@gmail.com>
2023-04-18 16:55:24 +0300

    modify js app and css files

--- js app.js
+++ js app.js
@@ -1,18 +1,18 @@
-let menuBar = document.querySelector('#menu-bar');
-let xIcon = document.querySelector('#x-icon');
-let popupList = document.querySelector('.popup-list');
-console.log(menuBar);
-xIcon.style.display = 'none'
-popupList.style.display = 'none'
+const menuBar = document.querySelector('#menu-bar');
+const xIcon = document.querySelector('#x-icon');
+const popupList = document.querySelector('.popup-list');
+
+xIcon.style.display = 'none';
+popupList.style.display = 'none';
 
 menuBar.addEventListener('click', () => {
-    xIcon.style.display = 'block'
-    popupList.style.display = 'block'
-    menuBar.style.display = 'none'
+  xIcon.style.display = 'block';
+  popupList.style.display = 'block';
+  menuBar.style.display = 'none';
 });
 
- xIcon.addEventListener('click', () => {
-    menuBar.style.display = 'block'
-    popupList.style.display = 'none'
-    xIcon.style.display = 'none'
+xIcon.addEventListener('click', () => {
+  menuBar.style.display = 'block';
+  popupList.style.display = 'none';
+  xIcon.style.display = 'none';
 });
