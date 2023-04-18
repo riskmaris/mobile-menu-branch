054bd9c43046f740776ff2b2e0b53d84f57c891d
riskmaris <mariskaps20@gmail.com>
2023-04-18 16:46:27 +0300

    Make changes

---
+++ js app.js
@@ -0,0 +1,18 @@
+let menuBar = document.querySelector('#menu-bar');
+let xIcon = document.querySelector('#x-icon');
+let popupList = document.querySelector('.popup-list');
+console.log(menuBar);
+xIcon.style.display = 'none'
+popupList.style.display = 'none'
+
+menuBar.addEventListener('click', () => {
+    xIcon.style.display = 'block'
+    popupList.style.display = 'block'
+    menuBar.style.display = 'none'
+});
+
+ xIcon.addEventListener('click', () => {
+    menuBar.style.display = 'block'
+    popupList.style.display = 'none'
+    xIcon.style.display = 'none'
+});
