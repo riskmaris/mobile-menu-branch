47077ebc144dc436ee6bb139f5a99b6a8a6c8f69
riskmaris <mariskaps20@gmail.com>
2023-04-22 08:36:19 +0300

    make changes

--- app.js
+++ app.js
@@ -73,3 +73,12 @@ function validateMessage() {
   messageError.innerHTML = '<i class="bi bi-check-circle-fill"></i>';
   return true;
 }
+
+function validateForm() {
+  if (!nameValidation() || !validateEmail() || !validateMessage()) {
+    buttonError.style.display = 'block';
+    buttonError.innerHTML = 'please fix error';
+    setTimeout(() => { buttonError.style.display = 'none'; }, 3000);
+    return false;
+  }
+}
