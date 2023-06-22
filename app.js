34608510b98832ac29de26ab3ebe8f1cc3525376
riskmaris <mariskaps20@gmail.com>
2023-06-22 21:04:17 +0300

    fix linter error

--- app.js
+++ app.js
@@ -88,35 +88,35 @@ buttonError.addEventListener('submit', validateForm);
 // ----------------local storage-------------------
 
 // Check if localStorage is supported by the browser
-if (typeof(Storage) !== "undefined") {
+if (typeof(Storage) !== 'undefined') {
   // Retrieve the form data from localStorage if it exists
-  let storedData = localStorage.getItem("formData");
+  let storedData = localStorage.getItem('formData');
   let formData = storedData ? JSON.parse(storedData) : {};
 
   // Populate the form fields with the stored data
-  document.getElementById("name-input").value = formData.name || "";
-  document.getElementById("your-email").value = formData.email || "";
-  document.getElementById("your-message").value = formData.message || "";
+  document.getElementById('name-input').value = formData.name || '';
+  document.getElementById('your-email').value = formData.email || '';
+  document.getElementById("your-message").value = formData.message || '';
 
   // Save the form data to localStorage whenever a field is changed
-  document.getElementById("name-input").addEventListener("input", saveFormData);
-  document.getElementById("your-email").addEventListener("input", saveFormData);
-  document.getElementById("your-message").addEventListener("input", saveFormData);
+  document.getElementById('name-input').addEventListener('input', saveFormData);
+  document.getElementById('your-email').addEventListener('input', saveFormData);
+  document.getElementById('your-message').addEventListener('input', saveFormData);
 
   // Save the form data to localStorage before the page is refreshed
-  window.addEventListener("beforeunload", saveFormData);
+  window.addEventListener('beforeunload', saveFormData);
 
   // Clear the form data from localStorage when the form is submitted
-  document.querySelector("form").addEventListener("submit", function() {
-    localStorage.removeItem("formData");
+  document.querySelector('form').addEventListener('submit', function() {
+    localStorage.removeItem('formData');
   });
 
   function saveFormData() {
     var data = {
-      name: document.getElementById("name-input").value,
-      email: document.getElementById("your-email").value,
-      message: document.getElementById("your-message").value
+      name: document.getElementById('name-input').value,
+      email: document.getElementById('your-email').value,
+      message: document.getElementById('your-message').value
     };
-    localStorage.setItem("formData", JSON.stringify(data));
+    localStorage.setItem('formData', JSON.stringify(data));
   }
 }
